c8ca3064665c6eec6d9bbb17adaf4ab01bfa5599
ecrum19 <elicrum88@gmail.com>
2025-09-25 12:42:13 +0200

    fixed CLI behavior and compiling failures

--- engines/query-sparql-remote-cache/lib/CliArgsHandlerRemoteCache.ts
+++ engines/query-sparql-remote-cache/lib/CliArgsHandlerRemoteCache.ts
@@ -9,27 +9,23 @@ export class CliArgsHandlerRemoteCache implements ICliArgsHandler {
     return argumentsBuilder
       .options({
         location: {
-          alias: 'loc',
           type: 'string',
           describe: 'Location of the remote cache',
           group: 'Required options:',
         },
         valueClauseBlockSize: {
-          alias: 'vcbs',
           type: 'number',
           default: 20000,
           describe: 'The number of VALUES clauses to group together in a single query to the remote cache.',
           group: 'Advanced options:',
         },
         valueClauseReduction: {
-          alias: 'vcr',
           type: 'boolean',
           default: true,
           describe: 'Whether to reduce the number of VALUES clauses sent to the remote cache by removing those that are already contained in the query.',
           group: 'Advanced options:',
         },
         failOnCacheMiss: {
-          alias: 'focm',
           type: 'boolean',
           default: false,
           describe: 'Whether to return an error if a query cannot be found in the remote cache.',
@@ -47,6 +43,12 @@ export class CliArgsHandlerRemoteCache implements ICliArgsHandler {
 
   public async handleArgs(args: Record<string, any>, context: Record<string, any>): Promise<void> {
     context['@comunica/remote-cache:location'] = args.location;
+    try {
+      new URL(args.location);
+      context['@comunica/remote-cache:location'] = { url: args.location };
+    } catch (error) {
+      context['@comunica/remote-cache:location'] = { path: args.location };
+    }
     if (args.valueClauseBlockSize !== 'void') {
       context['@comunica/remote-cache:valueClauseBlockSize'] = args.valueClauseBlockSize;
     }
